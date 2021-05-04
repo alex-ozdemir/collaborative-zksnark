9032ca80b68a84f77bec25ed67c3eccecf5827d9
Alex Ozdemir <aozdemir@hmc.edu>
2021-05-04 08:27:20 -0700

    Full marlin runs

--- mpc-snarks/analysis/data-collection/exp.zsh
+++ mpc-snarks/analysis/data-collection/exp.zsh
@@ -2,24 +2,32 @@
 set -e
 
 
-echo constraints,local_time,ark_local_time,mpc_time
-for lsteps in $(seq 0 20)
-#for steps in 1 3 10 30 100 300 1000 3000 10000 30000 100000
-#for steps in 1 3 10 30 100 300 1000 3000
+echo constraints,proof,infra,time
+for proof in groth16 marlin
 do
-    steps=$((2 ** $lsteps))
-    local_time=$(./scripts/bench.zsh local $steps)
-    local_time=$(units -t $local_time s)
-    ark_local_time=$(./scripts/bench.zsh ark-local $steps)
-    ark_local_time=$(units -t $ark_local_time s)
-    trials=5
-    mpc_net_time=0
-    for trial in $(seq 1 $trials)
+    for lsteps in $(seq 3 20)
+    #for lsteps in $(seq 3 12)
     do
-        mpc_trial_time=$(./scripts/bench.zsh mpc $steps)
-        mpc_trial_time=$(units -t $mpc_trial_time s)
-        mpc_net_time=$(($mpc_net_time + $mpc_trial_time))
+        steps=$((2 ** $lsteps))
+        trials=3
+        local_net_time=0
+        for trial in $(seq 1 $trials)
+        do
+            local_trial_time=$(./scripts/bench.zsh $proof local $steps)
+            local_trial_time=$(units -t $local_trial_time s)
+            local_net_time=$(($local_net_time + $local_trial_time))
+        done
+        local_time=$(($local_net_time / $trials))
+        echo ${steps},${proof},local,${local_time}
+
+        mpc_net_time=0
+        for trial in $(seq 1 $trials)
+        do
+            mpc_trial_time=$(./scripts/bench.zsh $proof mpc $steps)
+            mpc_trial_time=$(units -t $mpc_trial_time s)
+            mpc_net_time=$(($mpc_net_time + $mpc_trial_time))
+        done
+        mpc_time=$(($mpc_net_time / $trials))
+        echo ${steps},${proof},mpc,${mpc_time}
     done
-    mpc_time=$(($mpc_net_time / $trials))
-    echo ${steps},${local_time},${ark_local_time},${mpc_time}
 done
---
+++ mpc-snarks/analysis/plotting/exp.R
@@ -0,0 +1,10 @@
+library(tidyverse)
+d <- read_csv("./analysis/data/exp.csv")
+data <- d 
+ggplot(data=data, mapping = aes(y = time, x = constraints, color = infra, linetype=infra, shape=infra)) +
+  geom_line() +
+  geom_point(size=4) +
+  scale_y_continuous(trans = "log2") +
+  scale_x_continuous(trans = "log2") +
+  facet_wrap(vars(proof))
+ggsave("./analysis/plots/groth16.png")
