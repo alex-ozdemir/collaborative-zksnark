bfd8e32a8aadb5435595efa94d8e352f3bc0f4a7
Alex Ozdemir <aozdemir@hmc.edu>
2021-05-07 15:35:43 -0700

    partial results

--- mpc-snarks/analysis/data-collection/exp.zsh
+++ mpc-snarks/analysis/data-collection/exp.zsh
@@ -1,33 +1,25 @@
 #!/usr/bin/env zsh
 set -e
 
+cargo build --release --bin proof
 
-echo constraints,proof,infra,time
-for proof in groth16 marlin
+echo constraints,proof,infra,trial,time
+for trial in $(seq 1 3)
 do
     for lsteps in $(seq 3 20)
     #for lsteps in $(seq 3 12)
     do
-        steps=$((2 ** $lsteps))
-        trials=3
-        local_net_time=0
-        for trial in $(seq 1 $trials)
+        for proof in groth16 marlin plonk
         do
+            steps=$((2 ** $lsteps))
             local_trial_time=$(./scripts/bench.zsh $proof local $steps)
             local_trial_time=$(units -t $local_trial_time s)
-            local_net_time=$(($local_net_time + $local_trial_time))
-        done
-        local_time=$(($local_net_time / $trials))
-        echo ${steps},${proof},local,${local_time}
+            echo ${steps},${proof},local,$trial,${local_trial_time}
 
-        mpc_net_time=0
-        for trial in $(seq 1 $trials)
-        do
-            mpc_trial_time=$(./scripts/bench.zsh $proof mpc $steps)
-            mpc_trial_time=$(units -t $mpc_trial_time s)
-            mpc_net_time=$(($mpc_net_time + $mpc_trial_time))
+            steps=$((2 ** $lsteps))
+            local_trial_time=$(./scripts/bench.zsh $proof mpc $steps)
+            local_trial_time=$(units -t $local_trial_time s)
+            echo ${steps},${proof},mpc,$trial,${local_trial_time}
         done
-        mpc_time=$(($mpc_net_time / $trials))
-        echo ${steps},${proof},mpc,${mpc_time}
     done
 done
