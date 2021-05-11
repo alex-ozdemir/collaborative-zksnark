878ca529f7aea8c2e73c07595b6243acae76a25b
Alex Ozdemir <aozdemir@hmc.edu>
2021-05-10 21:59:57 -0700

    more script fixes

--- mpc-snarks/analysis/data-collection/remote-exp.zsh
+++ mpc-snarks/analysis/data-collection/remote-exp.zsh
@@ -6,8 +6,6 @@ other_host=$2
 party_n=$3
 infra=$4
 
-cargo build --release --bin proof
-
 echo constraints,proof,infra,trial,time
 for trial in $(seq 1 1)
 do
@@ -18,7 +16,7 @@ do
         do
             steps=$((2 ** $lsteps))
             trial_time=$(./scripts/remote_bench.zsh $self_host $other_host $party_n $proof $steps)
-            trial_time=$(units -t $local_trial_time s)
+            trial_time=$(units -t $trial_time s)
             echo ${steps},${proof},$infra,$trial,${trial_time}
         done
     done
