9d5f072d1dc0892abeb130f8da4a4b11940199e2
Alex Ozdemir <aozdemir@hmc.edu>
2021-05-10 22:00:50 -0700

    up data

--- mpc-snarks/analysis/data-collection/remote-exp.zsh
+++ mpc-snarks/analysis/data-collection/remote-exp.zsh
@@ -10,7 +10,7 @@ echo constraints,proof,infra,trial,time
 for trial in $(seq 1 1)
 do
     #for lsteps in $(seq 3 20)
-    for lsteps in $(seq 3 5)
+    for lsteps in $(seq 3 10)
     do
         for proof in groth16 marlin plonk
         do
