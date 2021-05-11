237ce81b541d0ef6aab18876f05c805d7ec9c095
Alex Ozdemir <aozdemir@hmc.edu>
2021-05-10 17:02:30 -0700

    more data in plots

--- mpc-snarks/analysis/plotting/exp.R
+++ mpc-snarks/analysis/plotting/exp.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 d <- read_csv("./analysis/data/exp.csv")
-data <- d 
+data <- d %>% group_by(proof, infra, constraints) %>% summarize(time=mean(time))
 ggplot(data=data, mapping = aes(y = time, x = constraints, color = infra, linetype=infra, shape=infra)) +
   geom_line() +
   geom_point(size=4) +
   scale_y_continuous(trans = "log2") +
   scale_x_continuous(trans = "log2") +
   facet_wrap(vars(proof))
-ggsave("./analysis/plots/exp.png", width=8, height=6)
+ggsave("./analysis/plots/exp.png", width=8, height=5)
