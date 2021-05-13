d8dbe7d5dfe664e435ab672ff81519672550f10c
Alex Ozdemir <aozdemir@hmc.edu>
2021-05-12 23:15:30 -0700

    Working coord remote

--- mpc-snarks/analysis/data-collection/coord-remote-exp.zsh
+++ mpc-snarks/analysis/data-collection/coord-remote-exp.zsh
@@ -27,6 +27,28 @@ fi
 
 bin_file=${bin:t}
 
+
 echo "Copying proof binary..."
 scp $bin $self_host:$bin_file
 scp $bin $other_host:$bin_file
+scp analysis/data-collection/remote-exp-out.zsh ${self_host}:run.zsh
+scp analysis/data-collection/remote-exp-out.zsh ${other_host}:run.zsh
+
+echo "Checking for units on each host..."
+ssh $self_host "units -t 1s us"
+ssh $other_host "units -t 1s us"
+
+echo "Checking for zsh on each host..."
+ssh $self_host "zsh --version"
+ssh $other_host "zsh --version"
+
+echo "Checking binary..."
+ssh $self_host "./$bin_file -p groth16 -c squaring --computation-size 10 local"
+ssh $other_host "./$bin_file -p groth16 -c squaring --computation-size 10 local"
+
+echo "Running experiments..."
+ssh $self_host "./run.zsh $self_host $other_host 0 $infra $bin_file" > out0 & ; pid0=$!
+ssh $other_host "./run.zsh $other_host $other_host 1 $infra $bin_file" | tee out1 & ; pid1=$!
+wait $pid0 $pid1
+
+echo Done
--- mpc-snarks/analysis/data-collection/remote-exp-out.zsh
+++ mpc-snarks/analysis/data-collection/remote-exp-out.zsh
@@ -10,15 +10,16 @@ LABEL="timed section"
 
 (
 echo constraints,proof,infra,trial,time
-for trial in $(seq 1 3)
+for trial in $(seq 1 1)
+#for trial in $(seq 1 3)
 do
-    for lsteps in $(seq 3 20)
-    #for lsteps in $(seq 3 10)
+    #for lsteps in $(seq 3 20)
+    for lsteps in $(seq 3 4)
     do
         for proof in groth16 marlin plonk
         do
             steps=$((2 ** $lsteps))
-            trial_time=$($BIN -p $proof -c squaring --computation-size $steps mpc --host $self_host --peer-host $other_host --party $party_n | grep -r "End: *$LABEL" | grep -r -o '[0-9][0-9.]*.s')
+            trial_time=$(./$BIN -p $proof -c squaring --computation-size $steps mpc --host $self_host --peer-host $other_host --party $party_n | grep -E "End: *$LABEL" | grep -E -o '[0-9][0-9.]*.s')
             trial_time=$(units -t $trial_time s)
             echo ${steps},${proof},$infra,$trial,${trial_time}
         done
