8df4b26634393ad81178dd075f009876ad2a5cd0
Alex Ozdemir <aozdemir@hmc.edu>
2021-05-31 10:36:26 -0700

    remove dead analysis code

--- mpc-algebra/analysis/plotting/groth16.R
+++
@@ -1,9 +0,0 @@
-library(tidyverse)
-d <- read_csv("./analysis/data/groth16.csv")
-data <- d %>% transmute(constraints=constraints, mpc=mpc_time, local=local_time) %>% pivot_longer(!constraints, names_to = "infra", values_to = "time")
-ggplot(data=data, mapping = aes(y = time, x = constraints, color = infra, linetype=infra, shape=infra)) +
-  geom_line() +
-  geom_point(size=4) +
-  scale_y_continuous(trans = "log2") +
-  scale_x_continuous(trans = "log2")
-ggsave("./analysis/plots/groth16.png")
--- mpc-algebra/scripts/local.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-set -e
-
-cargo build --release --bin proof -q 2> /dev/null
-
-BIN=./target/release/proof
-
-$BIN -c squaring --computation-size $1 local | rg 'End: *crypto' | rg -o '[0-9][0-9.]*.s'
