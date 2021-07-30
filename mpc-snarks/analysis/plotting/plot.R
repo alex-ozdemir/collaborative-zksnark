8fc7f91c017deade0aa5be185482d854c468a6e4
Alex Ozdemir <aozdemir@hmc.edu>
2021-07-30 13:03:54 -0700

    6pc, better labels

--- mpc-snarks/analysis/plotting/plot.R
+++ mpc-snarks/analysis/plotting/plot.R
@@ -1,13 +1,15 @@
 library(tidyverse)
 library(scales)
-d <- read_csv("./analysis/data/weak.csv")
+d <- bind_rows(read_csv("./analysis/data/weak.csv"), read_csv("./analysis/data/6pc.csv"))
 dd <- d
 dd$alg <- paste(dd$alg, dd$parties, sep="")
 dd <- dd %>%
   mutate(alg = ifelse(alg == "local1", "Single Prover", alg)) %>%
-  mutate(alg = ifelse(alg == "spdz2", "2PC: SPDZ", alg)) %>%
-  mutate(alg = ifelse(alg == "spdz3", "3PC: SPDZ", alg)) %>%
-  mutate(alg = ifelse(alg == "gsz3", "3PC: GSZ", alg))%>%
+  mutate(alg = ifelse(alg == "spdz2", "2PC: Malicious Maj.", alg)) %>%
+  mutate(alg = ifelse(alg == "spdz3", "3PC: Malicious Maj.", alg)) %>%
+  mutate(alg = ifelse(alg == "gsz3", "3PC: Honest Maj.", alg))%>%
+  mutate(alg = ifelse(alg == "spdz6", "6PC: Malicious Maj.", alg)) %>%
+  mutate(alg = ifelse(alg == "gsz6", "6PC: Honest Maj.", alg))%>%
   mutate(proof_system = ifelse(proof_system == "groth16", "Groth16", proof_system)) %>%
   mutate(proof_system = ifelse(proof_system == "marlin", "Marlin", proof_system)) %>%
   mutate(proof_system = ifelse(proof_system == "plonk", "Plonk", proof_system)) %>%
@@ -32,7 +34,7 @@ ggplot(dd, mapping = aes(x = size, y = time, color = alg, shape = alg)) +
                      labels = trans_format("log2", math_format(2^.x)),
                      minor_breaks = trans_breaks("log2", function(x) 2^x, 16),
                      ) +
-  scale_shape_manual(values = c(1, 2, 3, 4)) +
+  scale_shape_manual(values = c(1, 2, 3, 4, 5, 6)) +
   # scale_x_continuous(trans = "log2",
   #                    breaks = x_breaks) +
   labs(
